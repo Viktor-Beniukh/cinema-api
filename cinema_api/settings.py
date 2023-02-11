7ac28ca4e02b31509e67b8f723fbd90d759d00c2
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-02-11 19:39:30 +0200

    Added models & migrations

--- cinema_api/settings.py
+++ cinema_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "cinema",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -98,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 
 # Internationalization
