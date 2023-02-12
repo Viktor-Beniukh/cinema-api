1227207be0d88fb6a7c475b941d82c91fb4d1c5d
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-02-12 09:45:49 +0200

    Added SECRET_KEY to .env file

--- cinema_api/settings.py
+++ cinema_api/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z@l1r(mt7178cgd1n-2r4#in)f6=lyau31smx&nu_$#^v#(ct&"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,12 +89,6 @@ WSGI_APPLICATION = "cinema_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 
 DATABASES = {
     "default": {
